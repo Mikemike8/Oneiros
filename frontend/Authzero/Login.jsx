0c0ec6564c14820827461a301230790af62752e3
Mikemike8 <Michaeljh811@gmail.com>
2024-05-15 21:52:12 -0500

    AUTHUpdated.jsx

--- frontend/Authzero/Login.jsx
+++ frontend/Authzero/Login.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import React from 'react';
 
 const LoginButton = () => {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect, isAuthenticated } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  return (
+    !isAuthenticated && (
+      <button onClick={() => loginWithRedirect()}>
+        Sign In
+      </button>
+    )
+  );
 };
 
 export default LoginButton;
